4d4cd495c66a388b9666f355cb5818cc14c8559f
Angel Perea Arias <aperea@gamma03.aulas.gsyc.urjc.es>
2016-10-24 10:50:44 +0200

    Implemented json file

--- server.py
+++ server.py
@@ -6,12 +6,23 @@ Clase (y programa principal) para un servidor de eco en UDP simple
 
 import socketserver as SS
 import sys
+import time
+import json
 
 class SIPRegisterHandler(SS.DatagramRequestHandler):
     """
     Echo server class
     """
     Dicc = {}
+    def register2json(self):
+        '''
+        Rewrites the Dicctionary into a .json file
+        '''
+        json_file = open("registered.json", "w")
+        json.dump(self.Dicc, json_file, separators=(',', ': '), indent=4)
+        json_file.close()
+
+        
     def handle(self):
         self.wfile.write(b"Hemos recibido tu peticion")
         
@@ -24,11 +35,12 @@ class SIPRegisterHandler(SS.DatagramRequestHandler):
             Ip = self.client_address[0]
             if Exp == 0:
                 del self.Dicc[Dir]
+                self.register2json()
             elif Exp > 0:
                 self.Dicc[Dir] = {'address': Ip, 'Expires': Exp}
+                self.register2json()
             
-            self.wfile.write(b"200 OK")
-        print(self.Dicc)
+            self.wfile.write(b"SIP/2.0 200 OK\r\n\r\n")
 
 
 if __name__ == "__main__":
